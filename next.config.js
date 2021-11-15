697af5f3dc6a90121ce3b70a7686d91babb895c7
M K <kosmacieej@gmail.com>
2021-11-15 22:36:02 +0100

    Error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const { withContentlayer } = require('next-contentlayer');
 
-module.exports = withContentlayer()({
-  // Your Next.js config...
-});
+module.exports = withContentLayer()(
+  withBundleAnalyzer({
+    images: {
+      deviceSizes: [320, 420, 768, 1024, 1200, 1400, 1600],
+    },
+  })
+);
